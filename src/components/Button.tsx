e534c157aeaa0306e5fac1249c9963f4be7b97e0
mciccale <marcociccalebaztan@gmail.com>
2023-12-02 14:12:17 +0100

    removed experimental hook

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,14 @@
 // experimental feature
-import { useFormStatus } from 'react-dom';
+// import { useFormStatus } from 'react-dom';
 
 export default function Button() {
-  const { pending } = useFormStatus();
+  // const { pending } = useFormStatus();
 
-  const styles = `ml-2 h-20 w-20 rounded-full ${
-    pending ? 'bg-green-200' : 'bg-green-300 hover:bg-green-200'
-  } text-6xl`;
+  // const styles = `ml-2 h-20 w-20 rounded-full ${
+  //   pending ? 'bg-green-200' : 'bg-green-300 hover:bg-green-200'
+  // } text-6xl`;
+
+  const styles = `ml-2 h-20 w-20 rounded-full bg-green-300 hover:bg-green-200 text-6xl`;
 
   return (
     <button className={styles} type="submit">
--- src/utils/seed.ts
+++ src/utils/seed.ts
@@ -4,31 +4,26 @@ import dbConnect from '@/lib/dbConnect';
 
 const dummyTodos = [
   {
-    id: '1',
     title: 'Walk the dog',
     dueDate: '2023-12-10',
     done: false,
   },
   {
-    id: '2',
     title: 'Grocery shopping',
     dueDate: '2023-12-12',
     done: false,
   },
   {
-    id: '3',
     title: "Doctor's appointment",
     dueDate: '2023-12-15',
     done: false,
   },
   {
-    id: '4',
     title: 'Attend yoga class',
     dueDate: '2023-12-18',
     done: false,
   },
   {
-    id: '5',
     title: 'Read a book',
     dueDate: '2023-12-20',
     done: false,
@@ -38,8 +33,6 @@ const dummyTodos = [
 (async () => {
   await dbConnect();
   await TodoSchema.deleteMany({});
-  await TodoSchema.insertMany(
-    dummyTodos.map(({ title, dueDate, done }) => ({ title, dueDate, done }))
-  );
+  await TodoSchema.insertMany(dummyTodos);
   await mongoose.connection.close();
 })();
